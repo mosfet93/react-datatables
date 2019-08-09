d3edec33b252f385ebbfcfcbf796be53c85cad1d
mosfet93 <41214927+mosfet93@users.noreply.github.com>
2019-08-09 15:31:12 -0700

    added datatables

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Tbl from "./components/tbl";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    dataSet: []
+  };
+
+  componentDidMount = () => {
+    axios({
+      method: "get",
+      url: "http://localhost:3000/data.json",
+      crossDomain: true
+    }).then(resp => {
+      this.setState({ dataSet: resp.data });
+    });
+  };
+
+  render() {
+    return <Tbl dataSet={this.state.dataSet} />;
+  }
 }
 
 export default App;
